dcc2e18d3650b9f495f5641aa699055f09a5b0af
bowtharni-code <bowtharni@gmail.com>
2026-06-23 14:57:16 +0530

    Fix Render Settings

--- hungerfreeconnect/settings.py
+++ hungerfreeconnect/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lv%bjlk4a_6u@jpdy+ytzff67tva_u7qika6!za#r0jdlpn0_2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "hunger-free-connect.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
